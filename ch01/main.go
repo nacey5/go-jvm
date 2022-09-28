78d33c42edbd3bdb41caacec00138f96473b4574
DaHuangGO <1160124552@qq.com>
2022-09-29 00:50:29 +0800

    first commit 完成了类加载器的配置

---
+++ ch01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	cmd := parseCmd()
+	if cmd.versionFlag {
+		fmt.Printf("version 0.0.1")
+	} else if cmd.helpFlag || cmd.class == "" {
+		printUsage()
+	} else {
+		startJvm(cmd)
+	}
+}
---
+++ ch02/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"go-jvm/ch02/classpath"
+	"strings"
+)
+
+func main() {
+	cmd := parseCmd()
+	if cmd.versionFlag {
+		fmt.Printf("version 0.0.1")
+	} else if cmd.helpFlag || cmd.class == "" {
+		printUsage()
+	} else {
+		startJvm(cmd)
+	}
+}
+
+func startJvm(cmd *Cmd) {
+	cp := classpath.Parse(cmd.xJreOption, cmd.cpOption)
+	fmt.Printf("classpath:%v class:%v args:%v\n",
+		cp, cmd.class, cmd.args)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s\n", cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n", classData)
+}
